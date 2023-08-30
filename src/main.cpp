9ca33848cbd3ebd0531bff1e1f36012235476b8a
IJJA3141 <alex.estella.cj@gmail.com>
2023-08-30 17:05:56 +0200

    00.05.06 pre param rework

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,10 @@
 
 #include <pstl/glue_execution_defs.h>
 #include <string>
-#include <unordered_map>
+#include <map>
 
 int main(int _argc, char *_argv[]) {
   const char *err = "err";
-
-  Parser parser = Parser(err, {
-                                  "--help",
-                                  "--http-port",
-                                  "--https-port",
-                                  "--cert-path",
-                                  "--key-path",
-                                  "--no-save",
-                                  "-h",
-                              });
-
 
 
   return 0;
