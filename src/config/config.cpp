355b9a841cfb7725c38855cf9894f252ff749c5e
IJJA3141 <alex.estella.cj@gmail.com>
2024-07-19 15:25:56 +0200

    00.09.00 rework

---
+++ src/config/config.cpp
@@ -0,0 +1,8 @@
+#include "config.hpp"
+
+Config::Config(const int _argc, const char *const _argv[])
+{
+  for (int i = 0; i < _argc; i++) {
+    
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,136 @@
-#include "./tcp-server/tcp-server.hpp"
-#include "./tls-server/tls-server.hpp"
-#include "./params/params.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-#include <thread>
+struct Config {
+  bool verbose = false;
+  int httpPort = 80;
+  int httpsPort = 443;
+  std::string keyPath = "./data/cert/key.pem";
+  std::string certPath = "./data/cert/cert.pem";
+};
 
-int main(int _argc, char *_argv[]) {
-  Params params = Params(_argc, _argv);
+void loadConfig(const std::string _configPath, Config &_config)
+{
+  std::fstream stream;
+  stream.open(_configPath, std::ios::in);
 
-  http::TcpServer httpServer = http::TcpServer();
-  http::TlsServer httpsServer =
-      http::TlsServer(params.cert.c_str(), params.key.c_str());
+  if (stream.is_open()) {
+    std::string str;
+    while (std::getline(stream, str)) {
+      if (str.find("http="))
+        _config.httpPort = std::atoi(str.erase(0, 5).c_str());
+      else if (str.find("https="))
+        _config.httpPort = std::atoi(str.erase(0, 6).c_str());
+      else if (str.find("cert="))
+        _config.httpPort = std::atoi(str.erase(0, 5).c_str());
+      else if (str.find("key="))
+        _config.httpPort = std::atoi(str.erase(0, 4).c_str());
+      else
+        std::cout << "??" << std::endl;
+    };
 
-  httpServer.bind((char *)params.http.c_str());
-  httpsServer.bind((char *)params.https.c_str());
+    stream.close();
+  } else {
+    std::cout << "err" << std::endl;
+  }
 
-  std::thread http(&http::TcpServer::listen, httpServer);
-  std::thread https(&http::TcpServer::listen, httpsServer);
+  return;
+};
 
-  http.join();
-  https.join();
+void saveConfig(Config _config)
+{
+  std::fstream stream;
+  stream.open("./data/settings.txt", std::ios::out);
+
+  if (stream.is_open()) {
+    stream.clear();
+
+    stream << "http=" << _config.httpPort << "\nhttps=" << _config.httpsPort
+           << "\ncert=" << _config.certPath << "\nkey=" << _config.certPath << std::endl;
+
+    stream.close();
+  } else {
+    std::cout << "" << std::endl;
+  }
+
+  return;
+}
+
+int main(int _argc, char *_argv[])
+{
+  bool printHelp = false;
+  bool save = false;
+  bool reset = false;
+  std::string configPath = "./data/settings.txt";
+
+  Config conf;
+
+  // index at one to skip path to exec
+  for (int i = 1; i < _argc; i++) {
+    std::string str(_argv[i]);
+
+    if (str == "--help" || str == "-?") {
+      printHelp = true;
+      break;
+    } else if (str == "--config" || str == "-c") {
+      if (++i < _argc) {
+        configPath = std::string(_argv[i]);
+      } else {
+        std::cout << "config need path" << std::endl;
+        return 1;
+      }
+    } else if (str == "--cert-path") {
+      if (++i < _argc) {
+        conf.certPath = std::string(_argv[i]);
+      } else {
+        std::cout << "cert need path" << std::endl;
+        return 1;
+      }
+    } else if (str == "--key-path") {
+      if (++i < _argc) {
+        conf.keyPath = std::string(_argv[i]);
+      } else {
+        std::cout << "key need path" << std::endl;
+        return 1;
+      }
+    } else if (str == "--http-port" || str == "-h") {
+      if (++i < _argc) {
+        conf.httpPort = std::atoi(_argv[i]);
+      } else {
+        std::cout << "http need value" << std::endl;
+        return 1;
+      }
+    } else if (str == "--https-port" || str == "-p") {
+      if (++i < _argc) {
+        conf.httpsPort = std::atoi(_argv[i]);
+      } else {
+        std::cout << "https need value" << std::endl;
+        return 1;
+      }
+    } else if (str == "--save" || str == "-s")
+      save = true;
+    else if (str == "--verbose" || str == "-v")
+      conf.verbose = true;
+    else if (str == "--default" || str == "-d")
+      reset = true;
+    else {
+      std::cout << "?" << std::endl;
+      return 1;
+    }
+  }
+
+  if (printHelp) {
+    std::cout << "o_o" << std::endl;
+    return 0;
+  }
+
+  if (reset)
+    conf = Config();
+  else
+    loadConfig(configPath, conf);
+
+  if (save) saveConfig(conf);
 
   return 0;
 }
