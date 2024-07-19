b8adac89aeca66ac23d0ceccec649e129a73e1f4
IJJA3141 <alex.estella.cj@gmail.com>
2024-07-19 18:41:23 +0200

    00.09.01 param parser?

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -1,8 +1,78 @@
+#include <iostream>
+
 #include "config.hpp"
 
-Config::Config(const int _argc, const char *const _argv[])
+Parser::Parser(int _argc, char *_argv[])
 {
-  for (int i = 0; i < _argc; i++) {
-    
+  for (size_t i = 0; i < _argc; i++)
+    this->_args.push_back(_argv[i]);
+
+  return;
+}
+
+void Parser::parse(std::pair<std::string, std::string> _matches, std::function<void(void)> _lambda)
+{
+  for (auto arg : this->_args)
+    if (arg == _matches.first || arg == _matches.second) _lambda();
+
+  return;
+}
+
+void Parser::parse(std::pair<std::string, std::string> _matches, std::function<void(int)> _lambda)
+{
+  for (size_t i = 0; i < this->_args.size(); i++) {
+    if (this->_args[i] == _matches.first || this->_args[i] == _matches.second) {
+      if (++i < this->_args.size()) {
+        _lambda(stoi(this->_args[i]));
+        break;
+      } else {
+        std::cout << "err" << std::endl;
+        exit(1);
+      }
+    }
   }
+
+  return;
+}
+
+void Parser::parse(std::pair<std::string, std::string> _matches, std::function<void(bool)> _lambda)
+{
+  for (size_t i = 0; i < this->_args.size(); i++) {
+    if (this->_args[i] == _matches.first || this->_args[i] == _matches.second) {
+      if (++i < this->_args.size()) {
+        if (this->_args[i] == "true") {
+          _lambda(true);
+        } else if (this->_args[i] == "false") {
+          _lambda(false);
+        } else {
+          std::cout << "err" << std::endl;
+          exit(1);
+        }
+        break;
+      } else {
+        std::cout << "err" << std::endl;
+        exit(1);
+      }
+    }
+  }
+
+  return;
+}
+
+void Parser::parse(std::pair<std::string, std::string> _matches,
+                   std::function<void(std::string)> _lambda)
+{
+  for (size_t i = 0; i < this->_args.size(); i++) {
+    if (this->_args[i] == _matches.first || this->_args[i] == _matches.second) {
+      if (++i < this->_args.size()) {
+        _lambda(this->_args[i]);
+        break;
+      } else {
+        std::cout << "err" << std::endl;
+        exit(1);
+      }
+    }
+  }
+
+  return;
 }
--- src/config/config.hpp
+++ src/config/config.hpp
@@ -1,2 +1,17 @@
+#include <functional>
 #include <string>
+#include <vector>
 
+class Parser
+{
+private:
+  std::vector<std::string> _args;
+
+public:
+  Parser(int _argc, char *_argv[]);
+
+  void parse(std::pair<std::string, std::string> _matches, std::function<void(int)> _lambda);
+  void parse(std::pair<std::string, std::string> _matches, std::function<void(void)> _lambda);
+  void parse(std::pair<std::string, std::string> _matches, std::function<void(bool)> _lambda);
+  void parse(std::pair<std::string, std::string> _matches, std::function<void(std::string)> _lambda);
+};
