64a15d5c5e307e3d24a123ecaa767ed7df4567a1
IJJA3141 <alex.estella.cj@gmail.com>
2024-07-24 17:33:10 +0200

    00.09.02 pp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
+#include <string>
 
 #include "config/config.hpp"
 
 int main(int _argc, char *_argv[])
 {
-  Parser parser(_argc, _argv);
+  Config config;
+  // pas ouf
+  Parser parser(_argc, _argv,
+                {"--help", "-h", "--verbose", "-v", "--config", "-p", "--defautl-config", "-d",
+                 "--http-port", "--https-port", "--key", "-k", "--cert", "-c", "--save", "-s",
+                 "--reset-config", "-r"});
 
   parser.parse({"--help", "-h"}, []() {
-    std::cout << "help..." << std::endl;
+    std::cout << "help" << std::endl;
     exit(1);
   });
+  parser.parse({"--verbose", "-v"}, [&]() {
+    std::cout << "verbose" << std::endl;
+    config.verbose = true;
+  });
 
-  parser.parse<std::string>({"--config", "-c"}, [](auto path) { std::cout << path << std::endl; });
-  parser.parse<int>({"--config", "-c"}, [](auto path) { std::cout << path << std::endl; });
+  parser.parse({"--config", "-p"}, [&](std::string _configPath) {
+    if (config.verbose) std::cout << "" << std::endl;
+    config.configPath = _configPath;
+  });
+
+  config.load();
+
+  parser.parse({"--defautl-config", "-d"}, [&]() {
+    bool verbose = config.verbose;
+    if (config.verbose) std::cout << "" << std::endl;
+    config = Config();
+    config.verbose = verbose;
+  });
+
+  parser.parse({"--http-port", ""}, [&](int _port) {
+    if (config.verbose) std::cout << "" << std::endl;
+    config.httpPort = _port;
+  });
+  parser.parse({"--https-port", ""}, [&](int _port) {
+    if (config.verbose) std::cout << "" << std::endl;
+    config.httpPort = _port;
+  });
+  parser.parse({"--key", "-k"}, [&](std::string _path) {
+    if (config.verbose) std::cout << "" << std::endl;
+    config.key = _path;
+  });
+  parser.parse({"--cert", "-c"}, [&](std::string _path) {
+    if (config.verbose) std::cout << "" << std::endl;
+    config.cert = _path;
+  });
+  parser.parse({"--save", "-s"}, [&]() { config.save(); });
+  parser.parse({"--restore-config", "-r"}, []() { Config().save(); });
 
   return 0;
 }
