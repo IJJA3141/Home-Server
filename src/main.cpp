62732c5eab50d7260d88fcac1ae5c90ed239afc8
IJJA3141 <alex.estella.cj@gmail.com>
2024-07-25 17:21:44 +0200

    00.09.04 this works i gess

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,108 @@
+#include <any>
 #include <functional>
+#include <map>
 #include <string>
-#include <vector>
 
-class AbstractFunction
-{
-  enum Type { VOID, STRING, INT };
-  Type type_;
-
-  std::function<void(std::string)> fString_;
-  std::function<void(void)> fVoid_;
-  std::function<void(int)> fInt_;
-
-  AbstractFunction(std::function<void(std::string)> _func);
-  AbstractFunction(std::function<void(void)> _func);
-  AbstractFunction(std::function<void(int)> _func);
-
-  void operator()();
+struct Config {
 };
 
-AbstractFunction::AbstractFunction(std::function<void(std::string)> _func)
+struct AFn {
+  AFn(std::function<void(std::string)> _lambda);
+  AFn(std::function<void(void)> _lambda);
+  AFn(std::function<void(int)> _lambda);
+
+private:
+  enum Type {
+    STRING,
+    VOID,
+    INT,
+  };
+
+  Type type;
+
+  std::function<void(void)> voidFn_;
+  std::function<void(int)> intFn_;
+  std::function<void(std::string)> stringFn_;
+};
+
+AFn::AFn(std::function<void(void)> _lambda)
 {
-  this->type_ = AbstractFunction::STRING;
-  this->fString_ = _func;
+  this->type = AFn::Type::VOID;
+  this->voidFn_ = _lambda;
 
   return;
 };
 
-AbstractFunction::AbstractFunction(std::function<void(void)> _func)
+AFn::AFn(std::function<void(std::string)> _lambda)
 {
-  this->type_ = AbstractFunction::VOID;
-  this->fVoid_ = _func;
+  this->type = AFn::Type::STRING;
+  this->stringFn_ = _lambda;
 
   return;
 };
 
-AbstractFunction::AbstractFunction(std::function<void(int)> _func)
+AFn::AFn(std::function<void(int)> _lambda)
 {
-  this->type_ = AbstractFunction::INT;
-  this->fInt_ = _func;
+  this->type = AFn::Type::INT;
+  this->intFn_ = _lambda;
 
   return;
 };
 
-void AbstractFunction::operator()()
+class Parser
 {
-  switch (this->type_) {
-  case AbstractFunction::VOID:
-    this->fVoid_();
-  case AbstractFunction::STRING:
-    this->fString_("");
-  case AbstractFunction::INT:
-    this->fInt_(2);
-  default:
-    exit(-1);
-  }
+private:
+  std::map<std::string, AFn *> map_;
+
+public:
+  Parser();
+
+  template <typename... Args> void add(std::function<void(std::string)> _lambda, Args... args);
+  template <typename... Args> void add(std::function<void(void)> _lambda, Args... args);
+  template <typename... Args> void add(std::function<void(int)> _lambda, Args... args);
+  Config parse();
+};
+
+Parser::Parser(){};
+
+template <typename... Args> void Parser::add(std::function<void(void)> _lambda, Args... _keys)
+{
+  AFn *ptr = new AFn(_lambda);
+
+  for (const auto key : {_keys...})
+    this->map_[key] = ptr;
 
   return;
-}
+};
+
+template <typename... Args> void Parser::add(std::function<void(int)> _lambda, Args... _keys)
+{
+  AFn *ptr = new AFn(_lambda);
+
+  for (const auto key : {_keys...})
+    this->map_[key] = ptr;
+
+  return;
+};
+
+template <typename... Args>
+void Parser::add(std::function<void(std::string)> _lambda, Args... _keys)
+{
+  AFn *ptr = new AFn(_lambda);
+
+  for (const auto key : {_keys...})
+    this->map_[key] = ptr;
+
+  return;
+};
 
 int main(int argc, char *argv[])
 {
-  std::vector<AbstractFunction> vec;
-  
+  Parser par;
+
+  par.add([]() {}, "", "", "", "");
+  par.add([](int) {}, "", "", "", "");
+  par.add([](std::string) {}, "", "", "", "");
+
   return 0;
 }
