3a6c819e34fdfe32f499e290d8671bd5bfdaf298
IJJA3141 <alex.estella.cj@gmail.com>
2024-08-20 01:14:43 +0200

    00.10.03 parser headers

--- src/server/parser.cpp
+++ src/server/parser.cpp
@@ -74,54 +74,70 @@ Message parse(std::string _message)
 {
   Message message;
   message.failure = Message::Failure::NONE;
+  std::string cmd = _message.substr(0, _message.find("\r\n"));
 
-  switch (_message[0]) {
-  case 'G':
-    if (_message.compare(0, 3, "GET") == 0) {
+  size_t index = cmd.find(" ");
+
+  switch (index) {
+  case 3:
+    if (cmd.compare(0, 3, "GET") == 0) {
       message.cmd.method = Method::GET;
       break;
+    } else if (cmd.compare(0, 3, "POST") == 0) {
+      message.cmd.method = Method::PUT;
+      break;
     }
-  case 'H':
-    if (_message.compare(0, 4, "HEAD") == 0) {
+  case 4:
+    if (cmd.compare(0, 4, "HEAD") == 0) {
       message.cmd.method = Method::HEAD;
       break;
-    }
-  case 'P':
-    if (_message[1] == 'O') {
-      if (_message.compare(0, 4, "POST") == 0) {
-        message.cmd.method = Method::POST;
-        break;
-      }
-    } else if (_message[1] == 'U') {
-      if (_message.compare(0, 4, "POST") == 0) {
-        message.cmd.method = Method::POST;
-        break;
-      }
-    }
-  case 'D':
-    if (_message.compare(0, 6, "DELETE") == 0) {
-      message.cmd.method = Method::DELETE;
+    } else if (cmd.compare(0, 4, "POST") == 0) {
+      message.cmd.method = Method::POST;
       break;
     }
-  case 'C':
-    if (_message.compare(0, 7, "CONNECT") == 0) {
-      message.cmd.method = Method::CONNECT;
-      break;
-    }
-  case 'O':
-    if (_message.compare(0, 7, "OPTIONS") == 0) {
-      message.cmd.method = Method::OPTIONS;
-      break;
-    }
-  case 'T':
-    if (_message.compare(0, 5, "TRACE") == 0) {
+  case 5:
+    if (cmd.compare(0, 5, "TRACE") == 0) {
       message.cmd.method = Method::TRACE;
       break;
     }
+  case 6:
+    if (cmd.compare(0, 6, "DELETE") == 0) {
+      message.cmd.method = Method::DELETE;
+      break;
+    }
+  case 7:
+    if (cmd.compare(0, 7, "CONNECT") == 0) {
+      message.cmd.method = Method::CONNECT;
+      break;
+    } else if (cmd.compare(0, 7, "OPTIONS") == 0) {
+      message.cmd.method = Method::OPTIONS;
+      break;
+    }
   default:
-    ERR("");
     exit(1);
   };
 
+  message.cmd.path = cmd.substr(index + 1, cmd.rfind(" ") - index);
+  message.cmd.protocol = cmd.substr(cmd.rfind(" ") + 1);
 
+  std::string::size_type contentLength = _message.find("Content-Length: ");
+
+  if (contentLength != std::string::npos) {
+    contentLength += 16;
+    size_t i = contentLength;
+
+    while (_message[++i] != '\r')
+      ;
+
+    message.body = _message.substr(atoi(_message.substr(contentLength, i - contentLength).c_str()));
+
+    size_t ancer = i = cmd.size();
+    for(; i < _message.size(); i++)
+    {
+      
+
+    }
+  }
+
+  return message;
 };
