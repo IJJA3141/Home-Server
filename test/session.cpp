e3ead60d87cd98b10425a9c12fdddd8a83711df5
IJJA3141 <alex.estella.cj@gmail.com>
2025-06-24 15:35:40 +0200

    all test are good

--- test/session.cpp
+++ test/session.cpp
@@ -1,6 +1,6 @@
+#include "../src/config.hpp"
 #include "../src/log.hpp"
 #include "../src/network/auth.hpp"
-#include "../src/config.hpp"
 
 #include <filesystem>
 #include <fstream>
@@ -45,7 +45,7 @@ bool test_load()
 
     if ((i = cache.fetch(uuid)) == -1)
     {
-      // err("could not fetch user " + std::string(id));
+      err("could not fetch user " + std::string(id));
       return true;
     }
   }
@@ -56,8 +56,8 @@ bool test_load()
 
     if ((i = cache.fetch(uuid)) != -1)
     {
-      // err("fetched invalid uuid: " + std::string(id));
-      // err(cache[i].user);
+      err("fetched invalid uuid: " + std::string(id));
+      err(cache[i].user);
       return true;
     }
   }
@@ -74,7 +74,7 @@ bool test_load()
              "08b92265-9f6e-4ab0-bc47-618f5021fc8406/12/30:20:38:25.149326666user 8\n"
              "fc0244c8-ebc6-458a-a8bc-f4fdbe01082f06/12/30:20:38:25.149326666user 10\n")
   {
-    // err(str);
+    err(str);
 
     return true;
   }
@@ -96,7 +96,7 @@ bool test_gen()
   {
     if (cache.fetch(uuid.uuid) == -1)
     {
-      // err("user not found");
+      err("user not found");
       res = true;
       break;
     }
@@ -122,12 +122,12 @@ int session(int argc, char* argv[])
 {
   bool o = 0;
 
-  // if (o += test_load()) err("load failed");
-  // log("load passed");
-  // if (o += test_gen()) err("gen failed");
-  // log("gen passed");
-  // if (o += test_password()) err("password failed");
-  // log("password passed");
+  if (o += test_load()) err("load failed");
+  log("load passed");
+  if (o += test_gen()) err("gen failed");
+  log("gen passed");
+  if (o += test_password()) err("password failed");
+  log("password passed");
 
   return o;
 }
